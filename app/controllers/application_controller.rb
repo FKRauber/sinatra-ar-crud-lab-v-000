2a4f60579de4bf15450258e6415379c4a2d90c0e
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-08-20 20:59:27 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
   end
 
 
-  get '/posts/:id/delete' do
+  delete '/posts/:id/delete' do
     @post = Post.find_by_id(params[:id])
     @post.delete
     erb :deleted
